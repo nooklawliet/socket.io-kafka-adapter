10b3b9c5f10b696b1e413247a5850c4c095ce22d
nooklawliet <nooklawliet@gmail.com>
2023-02-09 14:48:07 +0700

    fix: index

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,17 +1,16 @@
 import { Adapter, BroadcastOptions } from 'socket.io-adapter';
 import { Consumer, Producer } from 'kafkajs';
 import { Namespace } from 'socket.io';
-export declare class KafkaAdapterOpts {
-    topics: Array<string>;
+export interface KafkaAdapterOpts {
     groupId: string;
 }
 export declare function createAdapter(consumer: Consumer, producer: Producer, opts: KafkaAdapterOpts): (nsp: Namespace) => KafkaAdapter;
 export declare class KafkaAdapter extends Adapter {
     private consumer;
     private producer;
-    topics: Array<string> | string;
+    topic: string;
     uid: any;
     constructor(nsp: Namespace, consumer: Consumer, producer: Producer, opts: KafkaAdapterOpts);
     onmessage(): Promise<void>;
-    broadcast(packet: any, opts: BroadcastOptions): void;
+    broadcast(packet: any, opts: BroadcastOptions): any;
 }
--- index.ts
+++ index.ts
@@ -5,13 +5,13 @@ import * as Debug from 'debug';
 import { Kafka, Consumer, Producer, CompressionTypes } from 'kafkajs';
 import { Namespace } from 'socket.io';
 
-const debug = new Debug('socket.io-kafka-adapter')
+const debug = new Debug('socket.io-kafka-adapter');
 const adapterStore: { [ns: string]: Adapter } = {}
-const DEFAULT_KAFKA_TOPIC = 'socketio';
+const DEFAULT_KAFKA_TOPIC = 'kafka_adapter';
 const uid = uid2(6);
 
-export class KafkaAdapterOpts {
-    topics: Array<string>
+export interface KafkaAdapterOpts {
+    // topics: Array<string>
     groupId: string
     // fromBeginning: boolean
 }
@@ -19,7 +19,6 @@ export class KafkaAdapterOpts {
 export function createAdapter(consumer: Consumer, producer: Producer, opts: KafkaAdapterOpts) {
     debug('create kafka adapter');
     return function(nsp: Namespace) {
-        debug('return kafka adapter');
         return new KafkaAdapter(nsp, consumer, producer, opts);
     }
 }
@@ -28,15 +27,15 @@ export class KafkaAdapter extends Adapter {
 
     private consumer: Consumer;
     private producer: Producer;
-    topics: Array<string> | string;
+    topic: string;
     uid: any;
 
     constructor(nsp: Namespace, consumer: Consumer, producer: Producer, opts: KafkaAdapterOpts) {
         super(nsp)
-        this.topics = opts.topics;
+        this.topic = DEFAULT_KAFKA_TOPIC;
         this.consumer = consumer;
         this.consumer.subscribe({
-            topics: opts.topics || [DEFAULT_KAFKA_TOPIC]
+            topic: this.topic
         }).then(() => this.onmessage())
         this.producer = producer;
         this.uid = uid2(6);
@@ -54,20 +53,12 @@ export class KafkaAdapter extends Adapter {
                         headers: message.headers,
                     });
                     const msg = message.value;
-                    debug('msg:', msg);
-                    debug('msg:', msg?.toString());
                     const args = msgpack.decode(msg);
                     debug('args:', args);
                     const [uid, packet, opts] = args;
-                    debug('uid:', uid);
-                    debug('this.uid:', this.uid);
-                    debug('packet:', packet);
-                    debug('opts:', opts);
-
                     if (this.uid === uid) {
                         return debug('ignore same uid');
                     }
-                    
                     opts.rooms = new Set(opts.rooms);
                     opts.except = new Set(opts.except);
                     super.broadcast(packet, opts);
@@ -77,6 +68,7 @@ export class KafkaAdapter extends Adapter {
             },
         });
     }
+
     broadcast(packet: any, opts: BroadcastOptions) {
         try {
             packet.nsp = this.nsp.name;
@@ -85,24 +77,20 @@ export class KafkaAdapter extends Adapter {
                 except: [...new Set(opts.except)],
                 flags: opts.flags,
             };
-            debug('rawOpts:', rawOpts);
+            debug('broadcast opt:', rawOpts);
             const msg = msgpack.encode([this.uid, packet, rawOpts]);
             let key = this.uid;
             if (opts.rooms && opts.rooms.size === 1) {
                 key = opts.rooms.keys().next().value;
             }
-            const topic = this.topics[0];
             const produceMessage = {
-                topic: topic,
+                topic: this.topic,
                 messages: [{
                     key: key,
                     value: msg
                 }],
-                acks: 0,
-                timeout: 30000,
-                compression: CompressionTypes.GZIP,
             }
-            debug('produce message:', produceMessage);
+            debug('producer send message:', produceMessage);
             this.producer.send(produceMessage);
             super.broadcast(packet, opts); 
         } catch (error) {
